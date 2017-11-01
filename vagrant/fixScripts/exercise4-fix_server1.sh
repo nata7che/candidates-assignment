8b64cfca81d768e9537c5135f1873741da53b42c
natashach <natasha@oribi.io>
2017-11-01 10:00:38 +0200

    ex2

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
+sed -i '1 s/^/192.168.100.11  server2\n/' /etc/hosts
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
+sed -i '1 s/^/192.168.100.10  server1\n/' /etc/hosts
